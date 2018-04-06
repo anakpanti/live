6f33190f95c17f4813f4c9d417fcd57eae9841fd
anakpanti <34655464+anakpanti@users.noreply.github.com>
2018-04-07 00:13:39 +0700

    df
    dsf

---
+++ nana.js
@@ -0,0 +1,34 @@
+<script type="text/javascript">
+<!-- 
+document.writeln(" <div class=\"video-container\"> "); 
+document.writeln(" <div id=\"tivi\"> "); 
+document.writeln(" <iframe allowfullscreen=\"true\" allowtransparency=\"true\" frameborder=\"0\" height=\"100%\" scrolling=\"no\" src=\"//www.streamingclas.cf/2017/11/bein-sport-2.html\" width=\"100%\"></iframe> "); 
+document.writeln(" <script type=\"text/javascript\"> "); 
+document.writeln("    function server2() { "); 
+document.writeln("       document.getElementById(\"tivi\").innerHTML = '<iframe allowfullscreen=\"true\" src=\"//www.streamingclas.cf/2017/11/bein-sport-2.html\" allowtransparency=\"true\" frameborder=\"0\" height=\"100%\" scrolling=\"no\" width=\"100%\"></iframe>'; "); 
+document.writeln("    } "); 
+document.writeln("    function server3() { "); 
+document.writeln("       document.getElementById(\"tivi\").innerHTML = '<iframe allowfullscreen=\"true\" src=\"//www.streamingclas.cf/2017/11/bein-sport-3.html\" allowtransparency=\"true\" frameborder=\"0\" height=\"100%\" scrolling=\"no\" width=\"100%\"></iframe>'; "); 
+document.writeln("    } "); 
+document.writeln("    function server4() { "); 
+document.writeln("       document.getElementById(\"tivi\").innerHTML = '<iframe allowfullscreen=\"true\" src=\"//www.streamingclas.cf/2017/11/bein-sport-4.html\" allowtransparency=\"true\" frameborder=\"0\" height=\"100%\" scrolling=\"no\" width=\"100%\"></iframe>'; "); 
+document.writeln("    } "); 
+document.writeln("    function server5() { "); 
+document.writeln("       document.getElementById(\"tivi\").innerHTML = '<iframe allowfullscreen=\"true\" src=\"//www.streamingclas.cf/2017/11/bein-sport-5.html\" allowtransparency=\"true\" frameborder=\"0\" height=\"100%\" scrolling=\"no\" width=\"100%\"></iframe>'; "); 
+document.writeln("    } "); 
+document.writeln("    function server6() { "); 
+document.writeln("       document.getElementById(\"tivi\").innerHTML = '<iframe allowfullscreen=\"true\" src=\"//www.streamingclas.cf/2017/11/bein-sport-6.html\" allowtransparency=\"true\" frameborder=\"0\" height=\"100%\" scrolling=\"no\" width=\"100%\"></iframe>'; "); 
+document.writeln("    } "); 
+document.writeln("    function server7() { "); 
+document.writeln("       document.getElementById(\"tivi\").innerHTML = '<iframe allowfullscreen=\"true\" src=\"//www.streamingclas.cf/2017/11/bein-sport-7.html\" allowtransparency=\"true\" frameborder=\"0\" height=\"100%\" scrolling=\"no\" width=\"100%\"></iframe>'; "); 
+document.writeln("    } "); 
+document.writeln("    function server8() { "); 
+document.writeln("       document.getElementById(\"tivi\").innerHTML = '<iframe allowfullscreen=\"true\" src=\"//www.streamingclas.cf/2017/11/bein-sport-8.html\" allowtransparency=\"true\" frameborder=\"0\" height=\"100%\" scrolling=\"no\" width=\"100%\"></iframe>'; "); 
+document.writeln("    } "); 
+document.writeln(" </script></div> "); 
+document.writeln(" </div> "); 
+document.writeln(" <div align=\"center\"> "); 
+document.writeln(" <a class=\"playbtn\" href=\"http://ball24live.blogspot.co.id/2018/02/ball-24-live.html\">Ball24Live</a> <a class=\"playbtn\" href=\"javascript:server2();\">Ball 2</a> <a class=\"playbtn\" href=\"javascript:server3();\">Ball 3</a> <a class=\"playbtn\" href=\"javascript:server4();\">Ball 4</a> <a class=\"playbtn\" href=\"javascript:server5();\">Ball 5</a><a class=\"playbtn\" href=\"javascript:server6();\">Ball 6</a><a class=\"playbtn\" href=\"javascript:server7();\">Ball 7</a><a class=\"playbtn\" href=\"javascript:server8();\">Ball 8</a></div> "); 
+document.writeln("  ");
+ // -->
+</script>
