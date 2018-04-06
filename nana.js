8a2843fe3989dbfd627df690e3db9f9b4fc2a2a9
anakpanti <34655464+anakpanti@users.noreply.github.com>
2018-04-07 00:45:19 +0700

    Update nana.js

--- nana.js
+++ nana.js
@@ -1,5 +1,1 @@
-<script type="text/javascript">
-<!-- 
-document.writeln(" <iframe allowfullscreen=\"true\" allowtransparency=\"true\" frameborder=\"0\" height=\"100%\" scrolling=\"no\" src=\"http://www.streamingclas.cf/2017/11/bein-sport-2.html\" width=\"100%\"></iframe> ");
- // -->
-</script>
+https://www.youtube.com/embed/ZwzY1o_hB5Y
